9ce1c3645dfb1bc0af07d97743c80bc9675c36a4
Sim Boon Long <simboonlong@gmail.com>
2020-12-22 11:06:13 +0800

    test: add tests

---
+++ src/components/Breadcrumb/Breadcrumb.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { Breadcrumbs } from "./Breadcrumb";
+
+describe("breadcrumbs", () => {
+  it("should render 0 breadcrumbs", () => {
+    render(<Breadcrumbs breadcrumbs={[]} />);
+    expect(screen.queryByTestId("breadcrumbs-list")).not.toBeInTheDocument();
+  });
+
+  it("should render 1 breadcrumb", () => {
+    render(<Breadcrumbs breadcrumbs={["abc"]} />);
+    expect(screen.getByText("abc")).toBeDefined();
+    expect(screen.getAllByTestId("breadcrumbs-item")).toHaveLength(1);
+  });
+
+  it("should render 2 breadcrumbs", () => {
+    render(<Breadcrumbs breadcrumbs={["abc", "xyz"]} />);
+    expect(screen.getByText("abc")).toBeDefined();
+    expect(screen.getByText("xyz")).toBeDefined();
+    expect(screen.getAllByTestId("breadcrumbs-item")).toHaveLength(2);
+  });
+});
--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -2,6 +2,24 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 
+interface BreadcrumbsProps {
+  breadcrumbs: string[];
+}
+
+export const Breadcrumbs: React.FunctionComponent<BreadcrumbsProps> = ({ breadcrumbs }) => {
+  if (breadcrumbs.length === 0) return null;
+  return (
+    <ul data-testid="breadcrumbs-list">
+      {breadcrumbs.map((item, index) => (
+        <li key={index} data-testid="breadcrumbs-item">
+          <span className="mx-2">/</span>
+          <span className="capitalize">{item}</span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 export const Breadcrumb: React.FunctionComponent = () => {
   const location = useLocation();
   const paths = location.pathname.split("/");
@@ -15,12 +33,7 @@ export const Breadcrumb: React.FunctionComponent = () => {
         <Link className="font-bold hover:underline" to="/">
           Tools
         </Link>
-        {breadcrumbs.map((item, index) => (
-          <span key={index}>
-            <span className="mx-2">/</span>
-            <span className="capitalize">{item}</span>
-          </span>
-        ))}
+        <Breadcrumbs breadcrumbs={breadcrumbs} />
       </div>
     </nav>
   );
