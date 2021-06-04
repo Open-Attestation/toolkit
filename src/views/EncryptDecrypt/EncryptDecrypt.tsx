a8306df335839f1e0d05fc8123a96f2b693ff558
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-06-04 13:20:13 +0800

    fix: provide example only for url

--- src/views/EncryptDecrypt/EncryptDecrypt.tsx
+++ src/views/EncryptDecrypt/EncryptDecrypt.tsx
@@ -1,23 +1,17 @@
 import { generateEncryptionKey, encryptString, decryptString } from "@govtechsg/oa-encryption";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FailedAlert } from "./../../components/Alert/Alert";
 
-const SAMPLE_DOC_URL =
-  "https://schemata.openattestation.com/sg/gov/moh/pdt-healthcert/1.0/healthcert-endorsed-document.json";
+const SAMPLE_ENCRYPTED_DOC =
+  "https://action.openattestation.com/?q=%7B%22type%22%3A%22DOCUMENT%22%2C%22payload%22%3A%7B%22uri%22%3A%22https%3A%2F%2Fapi-vaccine.storage.staging.notarise.io%2Fdocument%2F6cfbbcbf-85a1-4644-b61a-952c12376502%22%2C%22key%22%3A%222b1236683c3a842ed4a0bb032c1cf668e24bcaf8ce599aeef502c93cb628152c%22%2C%22permittedActions%22%3A%5B%22VIEW%22%2C%22STORE%22%5D%2C%22redirect%22%3A%22https%3A%2F%2Fwww.verify.gov.sg%2Fverify%22%7D%7D";
 
 export const EncryptDecrypt: React.FunctionComponent = () => {
   const [key, setKey] = useState(generateEncryptionKey());
   const [rawDocument, setRawDocument] = useState("");
-  const [url, setUrl] = useState("");
+  const [url, setUrl] = useState(SAMPLE_ENCRYPTED_DOC);
   const [encryptedDocument, setEncryptedDocument] = useState("");
   const [isValid, setValid] = useState(true);
   const [noticeMsg, setNoticeMsg] = useState("");
-
-  useEffect(() => {
-    fetch(SAMPLE_DOC_URL)
-      .then((res) => res.text())
-      .then((text) => setRawDocument(text));
-  }, []);
 
   return (
     <div className="container mx-auto py-6">
